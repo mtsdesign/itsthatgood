d40c0f8af47ed47e99a96f1ecc0f2c6a61f0d91a
Mike Simmonds <bikebimmonds@gmail.com>
2016-10-19 17:16:35 +0100

    better working version with div layout
    should be changed to a canvas element so it cam be saved.

--- script.js
+++ script.js
@@ -2,11 +2,46 @@ $( document ).ready(function() {
 
 	console.log('hello world');
 	var $msgArea = $('#msgArea'),
-		$inputBox = $('#inputBox')
+		$inputBox = $('#inputBox'),
+		$inputLength = $('#inputBox').text()
 
 	$inputBox.keyup(function () {
 		$msgArea.html($inputBox.val())
-		console.log($inputBox.val())
-	})
+		// console.log($inputBox.val())
+		console.log($(this).val().length + "the input")
+		
+		if ($(this).val().length > 0) {
+			$msgArea.removeClass();
+			$msgArea.addClass('largeText');
+		}
+
+		if ($(this).val().length > 12) {
+			$msgArea.removeClass();
+			$msgArea.addClass('smallText');
+		}
+
+
+		if ($(this).val().length > 17) {
+
+			$msgArea.removeClass();
+			$msgArea.addClass('smallerText');
+		};
+
+		
+
+		
+		})
+
+		
+	
+
+		
+
+		
+
+	
+
+	
+
 
 });
